d423899ce6c0e817b81b045f6b192f19b152295e
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-11-16 12:30:03 +0100

    remove concurrent execution

--- NPercept.App/Program.cs
+++ NPercept.App/Program.cs
@@ -3,7 +3,6 @@ using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace NPercept.App
 {
@@ -12,32 +11,26 @@ namespace NPercept.App
 		public static void Main (string[] args)
 		{
 			List<string> documents_list = new List<string>();
-			Object lockDocument = new Object();
 			List<string> words_list = new List<string>();
-			Object lockWord = new Object();
 			string text = System.IO.File.ReadAllText(@"hugo_les_miserables.txt");
 			string[] sentences = text.Split ('.');
 			string[] words;
 			int index = 0;
 			int word_total = 0;
-			Parallel.ForEach (sentences, sentence => {
+			foreach (string sentence in sentences) {
 				Console.WriteLine ("Processing {0}/{1} with a total of {2} words, and {3} unique words", index, sentences.Length, word_total, words_list.Count);
-				lock(lockDocument) {
-					documents_list.Add (sentence);
-				}
+				documents_list.Add (sentence);
 				words = sentence.Split (' ');
-				lock(lockWord) {
-					foreach (string word in words) {
-						if (!words_list.Contains (word)) {
-							words_list.Add (word.Trim ());
-						}
+				foreach (string word in words) {
+					if (!words_list.Contains (word)) {
+						words_list.Add (word.Replace(',', '\0').Trim ());
 					}
 				}
 				index++;
 				word_total += words.Length;
-			});
+			}
 
-			Matrix<int> words_relations;
+			//Matrix<int> words_relations;
 		}
 	}
 }
